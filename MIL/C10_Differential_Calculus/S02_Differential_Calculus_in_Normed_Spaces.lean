3f0d897b7ba729928786292df1e3d3d4b4ceafc5
havedifficultyinfindingnames <>
2024-07-10 20:51:31 +0800

    all finished, except C08

--- MIL/C10_Differential_Calculus/S02_Differential_Calculus_in_Normed_Spaces.lean
+++ MIL/C10_Differential_Calculus/S02_Differential_Calculus_in_Normed_Spaces.lean
@@ -92,23 +92,39 @@ example {ι : Type*} [CompleteSpace E] {g : ι → E →L[𝕜] F} (h : ∀ x, �
   -- sequence of subsets consisting of those `x : E` with norms `‖g i x‖` bounded by `n`
   let e : ℕ → Set E := fun n ↦ ⋂ i : ι, { x : E | ‖g i x‖ ≤ n }
   -- each of these sets is closed
-  have hc : ∀ n : ℕ, IsClosed (e n)
-  sorry
+  have hc : ∀ n : ℕ, IsClosed (e n) := fun i ↦
+    isClosed_iInter fun i ↦ isClosed_le (g i).cont.norm continuous_const
   -- the union is the entire space; this is where we use `h`
-  have hU : (⋃ n : ℕ, e n) = univ
-  sorry
+  have hU : (⋃ n : ℕ, e n) = univ := by
+    refine' eq_univ_of_forall fun x ↦ _
+    rcases h x with ⟨C, hC⟩
+    obtain ⟨m, hm⟩ := exists_nat_ge C
+    exact ⟨e m, mem_range_self m, mem_iInter.mpr fun i ↦ le_trans (hC i) hm⟩
   /- apply the Baire category theorem to conclude that for some `m : ℕ`,
        `e m` contains some `x` -/
-  obtain ⟨m, x, hx⟩ : ∃ m, ∃ x, x ∈ interior (e m) := sorry
-  obtain ⟨ε, ε_pos, hε⟩ : ∃ ε > 0, ball x ε ⊆ interior (e m) := sorry
-  obtain ⟨k, hk⟩ : ∃ k : 𝕜, 1 < ‖k‖ := sorry
+  obtain ⟨m, x, hx⟩ : ∃ m, ∃ x, x ∈ interior (e m) := nonempty_interior_of_iUnion_of_closed hc hU
+  obtain ⟨ε, ε_pos, hε⟩ : ∃ ε > 0, ball x ε ⊆ interior (e m) := isOpen_iff.mp isOpen_interior x hx
+  obtain ⟨k, hk⟩ : ∃ k : 𝕜, 1 < ‖k‖ := NormedField.exists_one_lt_norm 𝕜
   -- show all elements in the ball have norm bounded by `m` after applying any `g i`
-  have real_norm_le : ∀ z ∈ ball x ε, ∀ (i : ι), ‖g i z‖ ≤ m
-  sorry
-  have εk_pos : 0 < ε / ‖k‖ := sorry
+  have real_norm_le : ∀ z ∈ ball x ε, ∀ (i : ι), ‖g i z‖ ≤ m := by
+    intro z hz i
+    replace hz := mem_iInter.mp (interior_iInter_subset _ (hε hz)) i
+    apply interior_subset hz
+  have εk_pos : 0 < ε / ‖k‖ := div_pos ε_pos (zero_lt_one.trans hk)
   refine' ⟨(m + m : ℕ) / (ε / ‖k‖), fun i ↦ ContinuousLinearMap.opNorm_le_of_shell ε_pos _ hk _⟩
-  sorry
-  sorry
+  · exact div_nonneg (Nat.cast_nonneg _) εk_pos.le
+  · intro y le_y y_lt
+    calc
+      ‖g i y‖ = ‖g i (y + x) - g i x‖ := by rw [(g i).map_add, add_sub_cancel_right]
+      _ ≤ ‖g i (y + x)‖ + ‖g i x‖ := (norm_sub_le _ _)
+      _ ≤ m + m :=
+        (add_le_add (real_norm_le (y + x) (by rwa [add_comm, add_mem_ball_iff_norm]) i)
+          (real_norm_le x (mem_ball_self ε_pos) i))
+      _ = (m + m : ℕ) := by norm_cast
+      _ ≤ (m + m : ℕ) * (‖y‖ / (ε / ‖k‖)) :=
+        (le_mul_of_one_le_right (Nat.cast_nonneg _)
+          ((one_le_div <| div_pos ε_pos (zero_lt_one.trans hk)).2 le_y))
+      _ = (m + m : ℕ) / (ε / ‖k‖) * ‖y‖ := (mul_comm_div _ _ _).symm
 
 end
 
